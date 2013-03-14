039cedb7ccaa8dfe03ba9917d0cefdb925051700
Bob.Zhao <bob.zhao@infomax.com.tw>
2013-03-14 15:55:45 +0800

    add awesome/submenu/task_float icon

--- zenburn.lua
+++ zenburn.lua
@@ -80,9 +80,9 @@ theme.taglist_squares_unsel = theme.confdir .. "/icons/taglist/unsel.png"
 -- }}}
 
 -- {{{ Misc icons
---theme.awesome_icon           = theme.confdir .. "/icons/awesome.png"
---theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
---theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+theme.awesome_icon           = theme.confdir .. "/icons/awesome.png"
+theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
+theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
 -- }}}
 
 -- {{{ Layout icons
